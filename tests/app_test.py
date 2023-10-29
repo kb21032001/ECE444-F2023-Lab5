8e054f5f23c527bada64357e44f3b275bb1c36d2
kb21032001 <kshitij.bhardwaj21@gmail.com>
2023-10-29 16:44:47 -0400

    Implemented Search Functionality

--- tests/app_test.py
+++ tests/app_test.py
@@ -78,6 +78,14 @@ def test_messages(client):
 
 def test_delete_message(client):
     """Ensure the messages are being deleted"""
-    rv = client.get('/delete/1')
+    rv = client.get("/delete/1")
+    data = json.loads(rv.data)
+    assert data["status"] == 0
+    login(client, app.config["USERNAME"], app.config["PASSWORD"])
+    rv = client.get("/delete/1")
     data = json.loads(rv.data)
     assert data["status"] == 1
+
+def test_search(client):
+    response = client.get("/search/", content_type="html/text")
+    assert response.status_code == 200
